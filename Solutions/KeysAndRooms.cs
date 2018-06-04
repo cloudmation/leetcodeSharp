0cd4bb5b9f13740060adfc420cf9670b10aa9805
cloudmation <weijie@gmail.com>
2018-06-04 06:49:01 -0700

    added keys and rooms

---
+++ Solutions/KeysAndRooms.cs
@@ -0,0 +1,39 @@
+﻿namespace Solutions
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    // 841. Keys and Rooms - DSF
+    public class KeysAndRooms
+    {
+        public bool CanVisitAllRooms(IList<IList<int>> rooms)
+        {
+            var visited = new bool[rooms.Count()];
+            var nodesToVisit = new Stack<int>();
+            nodesToVisit.Push(0);
+            visited[0] = true;
+            while (nodesToVisit.Count() != 0)
+            {
+                int roomNumber = nodesToVisit.Pop();
+                foreach (var room in rooms[roomNumber])
+                {
+                    if (!visited[room])
+                    {
+                        visited[room] = true;
+                        nodesToVisit.Push(room);
+                    }
+                }
+            }
+
+            foreach (var v in visited)
+            {
+                if (!v)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Tests/KeysAndRoomsTest.cs
@@ -0,0 +1,40 @@
+﻿namespace Tests
+{
+    using System.Collections.Generic;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Solutions;
+
+    [TestClass]
+    public class KeysAndRoomsTest
+    {
+        [TestMethod]
+        public void TestFalse()
+        {
+            var solution = new KeysAndRooms();
+            IList<IList<int>> input = new List<IList<int>>();
+            input.Add(new List<int>() { 1, 3 });
+            input.Add(new List<int>() {3, 0, 1 });
+            input.Add(new List<int>() {2 });
+            input.Add(new List<int>() {0 });
+
+            var visited = solution.CanVisitAllRooms(input);
+            Assert.AreEqual(false, visited);
+        }
+
+        [TestMethod]
+        public void TestTrue()
+        {
+            var solution = new KeysAndRooms();
+            IList<IList<int>> input = new List<IList<int>>();
+            input.Add(new List<int>() { 1 });
+            input.Add(new List<int>() { 2 });
+            input.Add(new List<int>() { 3 });
+            input.Add(new List<int>());
+
+            var visited = solution.CanVisitAllRooms(input);
+            Assert.AreEqual(true, visited);
+        }
+    }
+}
