2e290f4397d238ede3c687a49c1fa16fd2055226
cloudmation <weijie@gmail.com>
2018-06-07 19:29:20 -0700

    added BinaryTreeZigzagLevelOrderTraversal

---
+++ Solutions/BinaryTreeZigzagLevelOrderTraversal.cs
@@ -0,0 +1,50 @@
+﻿namespace Solutions
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class BinaryTreeZigzagLevelOrderTraversal
+    {
+        public IList<IList<int>> ZigzagLevelOrder(TreeNode root)
+        {
+            var res = new List<IList<int>>();
+            if (root == null) return res;
+
+            var queue = new Queue<Tuple<TreeNode, int>>();
+            queue.Enqueue(Tuple.Create(root, 0));
+            while (queue.Count > 0)
+            {
+                var tuple = queue.Dequeue();
+                var node = tuple.Item1;
+                var currentLevel = tuple.Item2;
+
+                // Console.WriteLine($"level:{currentLevel}");
+                if (res.Count <= currentLevel)
+                {
+                    res.Add(new List<int>());
+                }
+
+                if (currentLevel % 2 == 0)
+                {
+                    res[currentLevel].Add(node.val);
+                }
+                else
+                {
+                    res[currentLevel].Insert(0, node.val);
+                }
+
+                if (node.left != null)
+                {
+                    queue.Enqueue(Tuple.Create(node.left, currentLevel + 1));
+                }
+
+                if (node.right != null)
+                {
+                    queue.Enqueue(Tuple.Create(node.right, currentLevel + 1));
+                }
+            }
+
+            return res;
+        }
+    }
+}
---
+++ Tests/BinaryTreeZigzagLevelOrderTraversalTest.cs
@@ -0,0 +1,62 @@
+﻿namespace Tests
+{
+    using System.Collections.Generic;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Solutions;
+
+    [TestClass]
+    public class BinaryTreeZigzagLevelOrderTraversalTest
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var sol = new BinaryTreeZigzagLevelOrderTraversal();
+            var root = new TreeNode(3)
+                           {
+                               left = new TreeNode(9)
+                                          {
+                                              left =
+                                                  new TreeNode(4)
+                                                      {
+                                                          left = new TreeNode(8),
+                                                          right = new TreeNode(9)
+                                                      },
+                                              right = new TreeNode(5)
+                                                          {
+                                                              left =
+                                                                  new TreeNode(7),
+                                                              right = new TreeNode(
+                                                                  12)
+                                                          }
+                                          },
+                               right = new TreeNode(20)
+                                           {
+                                               left =
+                                                   new TreeNode(15)
+                                                       {
+                                                           left =
+                                                               new TreeNode(45),
+                                                           right = new TreeNode(
+                                                               23)
+                                                       },
+                                               right = new TreeNode(7)
+                                                           {
+                                                               left = new TreeNode(
+                                                                   48),
+                                                               right =
+                                                                   new TreeNode(50)
+                                                           }
+                                           }
+                           };
+            var ret = sol.ZigzagLevelOrder(root);
+            IList<IList<int>> expected = new List<IList<int>>();
+            expected.Add(new List<int>() { 3 });
+            expected.Add(new List<int>() { 20, 9 });
+            expected.Add(new List<int>() { 4, 5, 15, 7 });
+            expected.Add(new List<int>() { 50, 48, 23, 45, 12, 7, 9, 8 });
+            Utlilitiy.AssertAreEqual(expected, ret);
+        }
+    }
+}
