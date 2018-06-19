2472e7345fe2b69f213f7ff0637af854d2f04d7d
cloudmation <weijie@gmail.com>
2018-06-18 20:14:08 -0700

    CombinationsSol

---
+++ Solutions/CombinationsSol.cs
@@ -0,0 +1,31 @@
+﻿namespace Solutions
+{
+    using System.Collections.Generic;
+
+    public class CombinationsSol
+    {
+        public IList<IList<int>> Combine(int n, int k)
+        {
+            var ret = new List<IList<int>>();
+            if (n < k) return ret;
+            this.Helper(n, k, 1, ret, new List<int>());
+            return ret;
+        }
+
+        private void Helper(int n, int k, int index, IList<IList<int>> ret, IList<int> comb)
+        {
+            if (comb.Count == k)
+            {
+                ret.Add(new List<int>(comb));
+                return;
+            }
+
+            for (int i = index; i <= n; i++)
+            {
+                comb.Add(i);
+                this.Helper(n, k, i + 1, ret, comb);
+                comb.RemoveAt(comb.Count - 1);
+            }
+        }
+    }
+}
---
+++ Tests/CombinationsTest.cs
@@ -0,0 +1,30 @@
+﻿namespace Tests
+{
+    using System.Collections.Generic;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Solutions;
+
+    [TestClass]
+    public class CombinationsTest
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var sol = new CombinationsSol();
+            var res = sol.Combine(4, 2);
+            Utlilitiy.AssertAreEqual(
+                new List<IList<int>>()
+                    {
+                        new List<int>() { 1, 2 },
+                        new List<int> { 1, 3 },
+                        new List<int> { 1, 4 },
+                        new List<int> { 2, 3 },
+                        new List<int> { 2, 4 },
+                        new List<int> { 3, 4 }
+                    },
+                res);
+        }
+    }
+}
