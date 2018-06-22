b7eb1dd3ea6cf4663534d300b0cd5fa5e6d7caea
cloudmation <weijie@gmail.com>
2018-06-22 07:03:09 -0700

    combinationsum

---
+++ Solutions/CombinationSumSol.cs
@@ -0,0 +1,43 @@
+﻿namespace Solutions
+{
+    using System.Collections.Generic;
+
+    public class CombinationSumSol
+    {
+        public IList<IList<int>> CombinationSum(int[] candidates, int target)
+        {
+            var res = new List<IList<int>>();
+            var current = new List<int>();
+            this.Helper(candidates, target, res, current, 0, 0);
+            return res;
+        }
+
+        private void Helper(int[] candidates, int target, IList<IList<int>> res, IList<int> current, int sum, int index)
+        {
+            if (sum == target)
+            {
+                res.Add(new List<int>(current));
+                return;
+            }
+
+            if (sum > target)
+            {
+                return;
+            }
+
+            if (index == candidates.Length)
+            {
+                return;
+            }
+
+            for (int i = index; i < candidates.Length; i++)
+            {
+                current.Add(candidates[i]);
+                sum += candidates[i];
+                this.Helper(candidates, target, res, current, sum, i);
+                sum -= candidates[i];
+                current.RemoveAt(current.Count - 1);
+            }
+        }
+    }
+}
---
+++ Tests/CombinationSumTest.cs
@@ -0,0 +1,22 @@
+﻿namespace Tests
+{
+    using System.Collections.Generic;
+    using System.Net;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Solutions;
+
+    [TestClass]
+    public class CombinationSumTest
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var sol = new CombinationSumSol();
+            var res = sol.CombinationSum(new[] { 2, 3, 6, 7 }, 7);
+            var expected = new List<IList<int>> { new List<int>() { 2, 2, 3 }, new List<int>() { 7 }};
+            Utlilitiy.AssertAreEqual(expected, res);
+        }
+    }
+}
