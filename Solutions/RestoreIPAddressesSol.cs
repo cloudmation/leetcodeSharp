a3b2a022ff53d6740e3854e3094ca4ca02b55996
cloudmation <weijie@gmail.com>
2018-06-26 07:40:10 -0700

    added backtracking method for restore ip question

--- Solutions/RestoreIPAddressesSol.cs
+++ Solutions/RestoreIPAddressesSol.cs
@@ -6,14 +6,14 @@
 
     public class RestoreIPAddressesSol
     {
-        public IList<string> RestoreIpAddresses(string s)
+        public IList<string> RestoreIpAddressesRecursion(string s)
         {
             var res = new List<string>();
-            Helper(s, res, string.Empty, 0, 0);
+            HelperRecursion(s, res, string.Empty, 0, 0);
             return res;
         }
 
-        private void Helper(string s, IList<string> res, string choosen, int index, int count)
+        private void HelperRecursion(string s, IList<string> res, string choosen, int index, int count)
         {
             if (count > 4)
             {
@@ -30,7 +30,53 @@
                 if (i + index > s.Length) break;
                 var item = s.Substring(index, i);
                 if ((item.StartsWith("0") && item.Length > 1) || (i == 3 && int.Parse(item) >= 256)) continue;
-                Helper(s, res, choosen + item + (count == 3 ? "" : "."), index + i, count + 1);
+                HelperRecursion(s, res, choosen + item + (count == 3 ? "" : "."), index + i, count + 1);
+            }
+        }
+
+        public IList<string> RestoreIpAddressesBackTracking(string s)
+        {
+            var res = new List<string>();
+            HelperBackTracking(s, res, new List<string>(), 0);
+            return res;
+        }
+
+        private void HelperBackTracking(string s, IList<string> res, List<string> choosen, int index)
+        {
+            if (choosen.Count > 4)
+            {
+                return;
+            }
+
+            if (choosen.Count == 4 && index == s.Length)
+            {
+                var builder = new StringBuilder();
+                for (int i = 0; i < choosen.Count - 1; i++)
+                {
+                    int value = -1;
+                    var parsed = int.TryParse(choosen[i], out value);
+                    if (parsed && value >= 0 && value <= 255)
+                    {
+                        builder.Append(choosen[i] + ".");
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+                builder.Append(choosen[choosen.Count - 1]);
+                res.Add(builder.ToString());
+                return;
+            }
+
+            for (int i = 1; i < 4; i++)
+            {
+                if (i + index > s.Length) break;
+                var item = s.Substring(index, i);
+                if ((item.StartsWith("0") && item.Length > 1) || (i == 3 && int.Parse(item) >= 256)) continue;
+                choosen.Add(item);
+                HelperBackTracking(s, res, choosen, index + i);
+                choosen.RemoveAt(choosen.Count - 1);
             }
         }
     }
--- Tests/RestoreIPAddressesTest.cs
+++ Tests/RestoreIPAddressesTest.cs
@@ -14,7 +14,17 @@
         public void Test1()
         {
             var sol = new RestoreIPAddressesSol();
-            var res = sol.RestoreIpAddresses("25525511135");
+            var res = sol.RestoreIpAddressesRecursion("25525511135");
+            CollectionAssert.AreEquivalent(
+                new List<string>() { "255.255.11.135", "255.255.111.35" }.ToArray(),
+                res.ToArray());
+        }
+
+        [TestMethod]
+        public void TestBackTracking()
+        {
+            var sol = new RestoreIPAddressesSol();
+            var res = sol.RestoreIpAddressesBackTracking("25525511135");
             CollectionAssert.AreEquivalent(
                 new List<string>() { "255.255.11.135", "255.255.111.35" }.ToArray(),
                 res.ToArray());
