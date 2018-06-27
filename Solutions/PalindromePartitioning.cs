e32f2329faf0c89bc217ec5df426692397a97641
cloudmation <weijie@gmail.com>
2018-06-27 06:54:28 -0700

    131. PalindromePartitioning

---
+++ Solutions/PalindromePartitioning.cs
@@ -0,0 +1,49 @@
+﻿namespace Solutions
+{
+    using System.Collections.Generic;
+
+    public class PalindromePartitioning
+    {
+        public IList<IList<string>> Partition(string s)
+        {
+            var res = new List<IList<string>>();
+            this.Helper(s, res, new List<string>(), 0);
+            return res;
+        }
+
+        private void Helper(string s, IList<IList<string>> res, List<string> current, int index)
+        {
+            if (s.Length == index)
+            {
+                res.Add(new List<string>(current));
+                return;
+            }
+
+            for (int i = index; i < s.Length; i++)
+            {
+                var str = s.Substring(index, i - index + 1);
+                if (this.isPalindrome(str))
+                {
+                    current.Add(str);
+                    this.Helper(s, res, current, i + 1);
+                    current.RemoveAt(current.Count - 1);
+                }
+            }
+        }
+
+        private bool isPalindrome(string p)
+        {
+            if (string.IsNullOrEmpty(p)) return false;
+            int i = 0;
+            int j = p.Length - 1;
+            while (i < j)
+            {
+                if (p[i] != p[j]) return false;
+                i++;
+                j--;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Tests/PalindromePartitioningTest.cs
@@ -0,0 +1,22 @@
+﻿namespace Tests
+{
+    using System.Collections.Generic;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Solutions;
+
+    [TestClass]
+    public class PalindromePartitioningTest
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var sol = new PalindromePartitioning();
+            var res = sol.Partition("abba");
+            var expected =
+                new List<IList<string>>() { new List<string>() { "a", "b", "b", "a" }, new List<string>(){"a", "bb", "a"}, new List<string>(){"abba"} };
+            Utlilitiy.AssertAreEqual(expected, res);
+        }
+    }
+}
