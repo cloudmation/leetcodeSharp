f05631ef15b0eb494663a6d9f102ef636c3920a1
cloudmation <weijie@gmail.com>
2018-07-04 07:40:23 -0700

    WorkBreakII

---
+++ Solutions/WordBreakIISol.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Solutions
+{
+    public class WordBreakIISol
+    {
+        private readonly IDictionary<int, IList<string>> map = new Dictionary<int, IList<string>>();
+
+        public IList<string> WordBreak(string s, IList<string> wordDict)
+        {
+
+            return Helper(new HashSet<string>(wordDict), s, 0);
+
+        }
+
+        private IList<string> Helper(HashSet<string> wordDict, string s, int index)
+        {
+            if (map.ContainsKey(index))
+            {
+                return map[index];
+            }
+
+            IList<string> res = new List<string>();
+            if (index == s.Length)
+            {
+                res.Add("");
+            }
+
+            for (int i = 1; i <= s.Length - index; i++)
+            {
+                var str = s.Substring(index, i);
+                if (wordDict.Contains(str))
+                {
+                    var list = Helper(wordDict, s, index + i);
+                    foreach (string l in list)
+                    {
+                        res.Add(str + (l.Equals("") ? "" : " ") + l);
+                    }
+                }
+            }
+
+            map[index] = res;
+            return res;
+        }
+    }
+}
---
+++ Tests/WorkBreakIITest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions;
+
+namespace Tests
+{
+    [TestClass]
+    public class WorkBreakIITest
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var sol = new WordBreakIISol(); 
+            var res = sol.WordBreak("catsanddog", new List<string>() { "cat", "cats", "and", "sand", "dog" });
+            CollectionAssert.AreEquivalent(new [] {"cat sand dog", "cats and dog"}, res.ToArray());
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            var sol = new WordBreakIISol();
+            var res = sol.WordBreak("pineapplepenapple"
+                , new List<string>() { "apple", "pen", "applepen", "pine", "pineapple" });
+            CollectionAssert.AreEquivalent(new[] {"pine apple pen apple",
+                "pineapple pen apple",
+                "pine applepen apple"}, res.ToArray());
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            var sol = new WordBreakIISol();
+            var res = sol.WordBreak("baaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+               , new List<string>() { "a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa" });
+            CollectionAssert.AreEquivalent(new string[] {}, res.ToArray());
+        }
+    }
+}
